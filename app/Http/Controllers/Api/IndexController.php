5547b398d82e5511b589a0c41eebabca8cdc1194
e <l>
2019-11-29 21:22:21 +0800

    订单

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -67,8 +67,11 @@ class IndexController extends Controller
             // 查询员工
             $userRes = User::where('openid', $data_all['openid'])->first();
             if ($userRes) $res = Workers::where('uid', $userRes['id'])->first();
-            if (!$res) return $this->error('目前不是员工');
-            header("Location:http://www.cqdaguanjia.com/#/home?openid=" . $data_all['openid']);
+            if (!$res) {
+                header("Location:http://www.cqdaguanjia.com/#/noStaff");
+                return;
+            }
+            header("Location:http://www.cqdaguanjia.com/#/staff?openid=" . $data_all['openid']);
             return;
         }
     }
--- app/Http/Controllers/Web/OrderController.php
+++ app/Http/Controllers/Web/OrderController.php
@@ -21,7 +21,6 @@ class OrderController extends Controller
     public function index(Request $request)
     {
         // 数据
-        $where = [];
         if (!empty($request->pay_type)) $where['pay_type'] = $request->pay_type;
         if (!empty($request->server_type)) $where['server_type'] = $request->server_type;
         if (!empty($request->phone)) $where['phone'] = $request->phone;
@@ -31,14 +30,18 @@ class OrderController extends Controller
         $limit = $this->newLimit($request->limit);
         $data = Order::query()->with('order_project');
         if (!empty($where)) $data = $data->where($where);
-        if (!empty($start_time)) $data = $data->where('start_time','<=', $start_time);
-        if (!empty($end_time)) $data = $data->where('end_time','>=', $end_time);
+        if (!empty($start_time)) $data = $data->where('start_time', '<=', $start_time);
+        if (!empty($end_time)) $data = $data->where('end_time', '>=', $end_time);
         $data = $data->offset(($page - 1) * $limit)
             ->limit($limit)
             ->orderBy('id', 'desc')
             ->get()
             ->toArray();
-        $count = Order::where($where)->count();
+        $count = new Order();
+        if (!empty($start_time)) $count = $count->where('start_time', '<=', $start_time);
+        if (!empty($end_time)) $count = $count->where('end_time', '>=', $end_time);
+        if (!empty($where)) $count = $count->where($where);
+        $count = $count->count();
         return $this->successPage($data, $count);
     }
 }
