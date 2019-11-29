db0bbae16aa47e965350e6c63c542b548e9b3078
e <l>
2019-11-30 01:18:31 +0800

    !@#$

--- app/Http/Controllers/Api/PayController.php
+++ app/Http/Controllers/Api/PayController.php
@@ -15,7 +15,65 @@ class PayController extends Controller
      * 密钥
      */
     protected $key = 'C4CA4238A0B923820DCC509A6F75849B';
-    public function jsapi(Request $request){
-        return app_path();
+
+    /**
+     * @param Request $request
+     */
+    public function jsapi(Request $request)
+    {
+        $path = app_path() . 'WxPay/example';
+        require_once $path . "../lib/WxPay.Api.php";
+        require_once $path . "WxPay.JsApiPay.php";
+        require_once $path . "WxPay.Config.php";
+        require_once $path . 'log.php';
+        //初始化日志
+        $logHandler = new CLogFileHandler("../logs/" . date('Y-m-d') . '.log');
+        $log = Log::Init($logHandler, 15);
+
+//打印输出数组信息
+        function printf_info($data)
+        {
+            foreach ($data as $key => $value) {
+                echo "<font color='#00ff55;'>$key</font> :  " . htmlspecialchars($value, ENT_QUOTES) . " <br/>";
+            }
+        }
+
+//①、获取用户openid
+        try {
+
+            $tools = new JsApiPay();
+            $openId = $tools->GetOpenid();
+
+            //②、统一下单
+            $input = new WxPayUnifiedOrder();
+            $input->SetBody("test");
+            $input->SetAttach("test");
+            $input->SetOut_trade_no("sdkphp" . date("YmdHis"));
+            $input->SetTotal_fee("1");
+            $input->SetTime_start(date("YmdHis"));
+            $input->SetTime_expire(date("YmdHis", time() + 600));
+            $input->SetGoods_tag("test");
+            $input->SetNotify_url("http://paysdk.weixin.qq.com/notify.php");
+            $input->SetTrade_type("JSAPI");
+            $input->SetOpenid($openId);
+            $config = new WxPayConfig();
+            $order = WxPayApi::unifiedOrder($config, $input);
+            echo '<font color="#f00"><b>统一下单支付单信息</b></font><br/>';
+            printf_info($order);
+            $jsApiParameters = $tools->GetJsApiParameters($order);
+
+            //获取共享收货地址js函数参数
+            $editAddress = $tools->GetEditAddressParameters();
+        } catch (Exception $e) {
+            Log::ERROR(json_encode($e));
+        }
+//③、在支持成功回调通知中处理成功之后的事宜，见 notify.php
+        /**
+         * 注意：
+         * 1、当你的回调地址不可访问的时候，回调通知会失败，可以通过查询订单来确认支付是否成功
+         * 2、jsapi支付时需要填入用户openid，WxPay.JsApiPay.php中有获取openid流程 （文档可以参考微信公众平台“网页授权接口”，
+         * 参考http://mp.weixin.qq.com/wiki/17/c0f37d5704f0b64713d5d2c37b468d75.html）
+         */
+
     }
 }
